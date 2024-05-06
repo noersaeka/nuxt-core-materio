50fa981fcdd0e8b71c126409fd788e846cccfdfc
Noersa Eka Khustana <noersa.eka@koltiva.com>
2024-05-07 02:26:45 +0700

    te

--- node_modules/@sidebase/nuxt-auth/dist/runtime/composables/refresh/useAuthState.d.ts
+++
@@ -1,9 +0,0 @@
-import { ComputedRef } from 'vue';
-import { CookieRef } from '#app';
-import { useAuthState as useLocalAuthState } from '../local/useAuthState';
-type UseAuthStateReturn = ReturnType<typeof useLocalAuthState> & {
-    rawRefreshToken: CookieRef<string | null>;
-    refreshToken: ComputedRef<string | null>;
-};
-export declare const useAuthState: () => UseAuthStateReturn;
-export default useAuthState;
