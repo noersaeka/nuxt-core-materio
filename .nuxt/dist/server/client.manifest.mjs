d96e18e8da78e341993d531582535bf29952d8bf
Noersa Eka Khustana <noersa.eka@koltiva.com>
2024-05-07 07:51:26 +0700

    remove nuxt build folder

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/noersa.eka/Development/Playground/nuxt-core-materio/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/types/auth.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '#auth' {
-  const getServerSession: typeof import('/Users/noersa.eka/Development/Playground/nuxt-core-materio/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').getServerSession
-  const getToken: typeof import('/Users/noersa.eka/Development/Playground/nuxt-core-materio/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').getToken
-  const NuxtAuthHandler: typeof import('/Users/noersa.eka/Development/Playground/nuxt-core-materio/node_modules/@sidebase/nuxt-auth/dist/runtime/server/services').NuxtAuthHandler
-
-}
